c11a63e937b624e7f994fd7f74712e3bd42b423d
Da Woon Lee <daniel.lee9415@gmail.com>
2018-10-05 18:24:03 -0400

    updated readme

--- src/App.test.js
+++ src/App.test.js
@@ -10,25 +10,17 @@ configure({ adapter: new Adapter() })
 
 describe('Weather App Tests', function() {
   describe('API calls', function() {
-    // it('should return a list of weather data', async () => {
-    //   let weatherData = await fetch(
-    //     'http://api.openweathermap.org/data/2.5/forecast?zip=10465&APPID=a4aaf9e2c3889a342be449d98c9f64d3'
-    //   )
-    //     .then(res => {
-    //       return res.json()
-    //     })
-    //     .then(res => {
-    //       return res.list
-    //     })
-    //   assert.equal(weatherData.length, 40)
-    // })
-  })
-  describe('App component testing', function() {
-    it('renders welcome message', function() {
-      const component = renderer.create(<Weather />).toJSON()
-      console.log(component.children)
-      // console.log(component.props)
-      // assert.equal(component.state, 40)
+    it('should return a list of weather data', async () => {
+      let weatherData = await fetch(
+        'http://api.openweathermap.org/data/2.5/forecast?zip=10465&APPID=a4aaf9e2c3889a342be449d98c9f64d3'
+      )
+        .then(res => {
+          return res.json()
+        })
+        .then(res => {
+          return res.list
+        })
+      assert.equal(weatherData.length, 40)
     })
   })
 })
